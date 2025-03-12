e5438353f5110e09162e5573c7f3174705738c9a
CrstnBhrqz <139578333+CrstBhrqz@users.noreply.github.com>
2025-03-11 22:20:42 -0500

    Enhance authentication system with user role management and admin dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
+import os
 from flask import Flask, flash, redirect, render_template, request, url_for
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 from app.models.user import User, users_db
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-
 app = Flask(__name__, template_folder='views') 
+app.secret_key= os.urandom(24)
 
 login_manager = LoginManager(app)
 
@@ -25,25 +23,34 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-       
-        if username in users_db and users_db[username]['password'] == password:
-            user = User(username)
-            login_user(user)
-            return redirect(url_for('success'))
-        
-        flash('Credenciales inválidas')
-    return render_template('login.html')
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@app.route('/success')
-@login_required
-def success():
-    return render_template('success.html', username=current_user.id)
+        # Buscar usuario en la base de datos
+        user = next((u for u in users_db.values() if u.username == username and u.password == password), None)
+
+        if user:
+            login_user(user)  # Iniciar sesión
+            flash('Inicio de sesión exitoso', 'success')
+            print('Inicio de sesión exitoso')
+            return redirect(url_for('success', username=user.username, es_admin=user.es_admin)) 
+        else:
+            flash('Usuario o contraseña incorrectos', 'danger')
+
+    return render_template('login.html')  # Página de login
+
+
+@app.route('/success/<username>/<es_admin>')
+def success(username, es_admin):
+
+    print('Inicio de sesión exitoso')
+    print(username)
+    print(es_admin)
+    if es_admin == 'True':
+        return render_template('admin.html', username=username, es_admin=es_admin)
+    return render_template('sucess.html', username=username,)
 
 @app.route('/logout')
-@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
--- app/models/user.py
+++ app/models/user.py
@@ -2,16 +2,17 @@ from flask_login import UserMixin
 
 
 class User(UserMixin):
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, password, es_admin):
         self.id = id
         self.username = username
         self.password = password
+        self.es_admin = es_admin
 
 # base de datos de usuarios ejemplo
 
 users_db = {
-    1: User(1, 'admin', 'admin'),
-    2: User(2, 'user', 'user'),
-    3: User(3, 'user2', 'user2'),
-    4: User(4, 'user3', 'user3'),
+  "zeus": User(1, 'admin', 'admin', True),
+    "hera": User(2, 'user', 'user', False),
+    "poseidon": User(3, 'user2', 'user2', False),
+    "apollo": User(4, 'user3', 'user3', False),
 }
